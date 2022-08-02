0265bb6b9c914d02a6bd0b7219a8ee37d70e694d
olajhidey <olajideawoyinka@gmail.com>
2022-08-02 14:59:53 +0100

    added workflows and new feature

--- src/main/java/client/CapitalClient.java
+++ src/main/java/client/CapitalClient.java
@@ -4,14 +4,18 @@ import java.util.Locale;
 
 public class CapitalClient {
 
-    public static String capitalLetter(String word){
+    public static String capitalLetter(String word) {
         //Trim letter before capitalizing the whole word
         return word.trim().toUpperCase();
     }
 
-    public static String lowerLetter(String word){
+    public static String lowerLetter(String word) {
         // Trim the letter before decapitalizing the word
         return word.trim().toLowerCase();
+    }
+
+    public static String[] listOfWords(String word, String by) {
+        return word.split(by);
     }
 
 }
